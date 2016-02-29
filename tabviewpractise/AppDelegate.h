b4b5da28b458137b1bc11e8867105e21eb0625b5
Student 1 <student1@josephs-macbook-air.local>
2016-02-29 13:09:02 -0500

    Initial Commit

---
+++ tabviewpractise/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  tabviewpractise
+//
+//  Created by Student 1 on 2/29/16.
+//  Copyright © 2016 Student 1. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ tabviewpractise/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  tabviewpractise
+//
+//  Created by Student 1 on 2/29/16.
+//  Copyright © 2016 Student 1. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
